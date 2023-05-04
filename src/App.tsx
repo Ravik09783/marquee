858122709cf305d787fc70b0b06e1b3ca868abd0
Ravi <ravi09783@gmail.com>
2023-05-04 15:35:31 +0530

    new marquee

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Login from './login/Login';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello world</h1>
+      <Login />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/login/Login.tsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+
+  const handleLogin = async (e: { preventDefault: () => void; }) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch('YOUR_API_LOGIN_URL', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email, password }),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        // Store the authentication token in local storage
+        localStorage.setItem('token', data.token);
+        // Redirect the user to the dashboard
+        navigate('/dashboard');
+      } else {
+        throw new Error('Invalid email or password');
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <form onSubmit={handleLogin}>
+      <input
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        placeholder="Email"
+      />
+      <input
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder="Password"
+      />
+      <button type="submit">Login</button>
+    </form>
+  );
+};
+
+export default Login;
